a4cb75148c6f239440eec4fa56a5f70276b41540
draco987987 <organai.b@gmail.com>
2025-04-03 09:38:41 +0100

    Clean data and visualizations

--- prueba.py
+++ prueba.py
@@ -7,16 +7,22 @@ file_path = "C:\\Users\\organ\\Desktop\\Code institute\\python\\test2-code-insti
 df = pd.read_csv(file_path)
 print(df.head(5))
 print(df.info())
+
+
 df.drop_duplicates(inplace = True) #eliminate duplicates
 df.dropna(inplace = True) #eliminate NaN values
 columns_to_drop = ["Chest_Pain_Type", "Resting_ECG", "Exercise_Induced_Angina", "Slope", "Thalassemia"] #Make a list of columns to drop
 df.drop(columns = columns_to_drop, inplace = True) #eliminate columns that are not useful for the analysis
 print(df.columns) #show the columns of the dataframe
+
+
 df["Gender"] = df["Gender"].map({"Male": 1, "Female": 0}) #convert categorical to numerical
 df["Smoking"] = df["Smoking_History"].map(lambda x: 1 if x in ["Former", "Current"] else 0) #convert categorical to numerical
 df["Alcohol_consumption"] = df["Alcohol_Consumption"].map({"None": 0, "Moderate": 1, "Heavy": 2}) #convert categorical to numerical
 df["Physical_Activity_Level"] = df["Physical_Activity_Level"].map({"Low": 1, "Moderate": 2, "High": 3}) #convert categorical to numerical
 print(df.dtypes) #check the data types of the columns
+
+
 df.to_csv("Dataset/Cleaned/clean_Heart_data.csv", index = False) #save the cleaned data to a new csv file
 df = pd.read_csv("Dataset/Cleaned/clean_Heart_data.csv") #read the cleaned data
 print(df.head(5)) #show the first 5 rows of the cleaned data
